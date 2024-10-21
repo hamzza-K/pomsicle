0fb86785903dfc75a5cdfb7b5daa72022d4afd65
Hamzza K <hk151817@gmail.com>
2024-10-21 22:05:50 +0500

    loads inventory

--- config.py
+++ config.py
@@ -1,11 +1,18 @@
 import os
+import sys
 import configparser
 
 
 def config(translator: str = 'pomsicle') -> list:
     """Returns the project credentials/configurations"""
+    
+    if getattr(sys, 'frozen', False):  # Check if running as an executable (.exe)
+        base_path = sys._MEIPASS
+    else:
+        base_path = os.path.dirname(__file__)
+
     config = configparser.ConfigParser()
-    config_file = os.path.join(os.path.dirname(__file__), 'config/config.cfg')
+    config_file = os.path.join(base_path, 'config/config.cfg')
     config.read(config_file)
     settings = config[translator]
 
