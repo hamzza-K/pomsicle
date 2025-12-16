6b6154afadb6e857005e3ec9b0e752970dbc5fe4
Hamzza <hk151817@gmail.com>
2025-12-16 18:32:29 +0500

    adding components information for agentic retrieval

--- agentic/config.py
+++ agentic/config.py
@@ -1,15 +1,15 @@
 import os
 import sys
 import configparser
+from configparser import SectionProxy
 
 
-def config(translator: str = 'pomsicle') -> list:
+def config(translator: str = 'pomsicle') -> SectionProxy:
     """Returns the project credentials/configurations"""
     
+    base_path = os.path.dirname(__file__)
     if getattr(sys, 'frozen', False):  # Check if running as an executable (.exe)
-        base_path = sys._MEIPASS
-    else:
-        base_path = os.path.dirname(__file__)
+        pass # TODO: Handle frozen executable case
 
     config = configparser.ConfigParser()
     config_file = os.path.join(base_path, 'config/config.cfg')
